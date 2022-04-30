02579f7dc1ae052589a0e346b95fb29573c37af9
Yusfuldev <owokoniran93@gmail.com>
2022-05-01 00:38:37 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/header/Nav";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <h1>Hello</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          {/* <Route path="" */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
